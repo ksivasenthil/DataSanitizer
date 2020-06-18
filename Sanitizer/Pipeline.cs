33f948f270eead673bb093d6d2471bec4af51943
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2020-06-18 20:35:15 +0530

    # Code updae * added exceptions to fail the test upfront

--- Sanitizer/Pipeline.cs
+++ Sanitizer/Pipeline.cs
@@ -1,22 +1,25 @@
 namespace Sanitizer
 {
+    using System;
     using Sanitizer.Contract;
     public class Pipeline
     {
         public Pipeline()
         {
-
+            throw new NotImplementedException();
         }
 
         public void AddProcessor(IProcessor processor)
         {
-
+            throw new NotImplementedException();
         }
 
         public void RemoveProcessor(int id)
         {
-
+            throw new NotImplementedException();
         }
+
+
 
     }
 }
