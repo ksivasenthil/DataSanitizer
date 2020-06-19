f49cd1a79d096229ef865979780e0a697c38790d
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2020-06-19 19:00:48 +0530

    # Code update * demonstrated the usage of the pipeline with an example.

--- Sanitizer/Program.cs
+++ Sanitizer/Program.cs
@@ -14,18 +14,32 @@
             string fileContent = File.ReadAllText(PathOfFileToSaintize);
             return fileContent;
         }
+        public Pipeline SetupPipeline()
+        {
+            StringProcessor removeQuotes = new StringProcessor("&quot;", 0);
+            StringProcessor removeStartingSpaces = new StringProcessor(@"^\s*\r\n\s*\b",1);
+            StringProcessor removeTrailingSpaces = new StringProcessor(@"\b\s*\r\n\s*$",2);
 
+            Pipeline sanitizeJsonKeys = new Pipeline();
+            sanitizeJsonKeys.AddProcessor(removeQuotes);
+            sanitizeJsonKeys.AddProcessor(removeStartingSpaces);
+            sanitizeJsonKeys.AddProcessor(removeTrailingSpaces);
+
+            return sanitizeJsonKeys;
+        }
         public static void Main(string[] args)
         {
             Program client = new Program();
             string jsonText = client.DirtyFileText();
             JObject data = JObject.Parse(jsonText);
             JObject modifiedData = new JObject();
+            Pipeline configuredPipeline = client.SetupPipeline();
             foreach (KeyValuePair<string, JToken> item in data)
             {
                 string key = item.Key;
                 Console.WriteLine(key);
-                modifiedData.Add(key.Trim(), data[key]);
+                string sanitizedKey = configuredPipeline.Process(key);
+                modifiedData.Add(sanitizedKey, data[key]);
             }
 
             foreach (KeyValuePair<string, JToken> item in modifiedData)
