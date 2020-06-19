e4585c414daf9a41a10ad510ef93751bad432bdc
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2020-06-19 19:28:32 +0530

    # [UNSTABLE] Code update * added test case for the validation of replacement text

--- Sanitizer.Test/ReplaceFeatureTest.cs
+++ Sanitizer.Test/ReplaceFeatureTest.cs
@@ -26,5 +26,25 @@ namespace Sanitizer.Test
 
             Assert.Equal(" HelloWorld ", resultOfProcessing);
         }
+
+        [Fact]
+        public void ReplacementStringIsUsed()
+        {
+            string input = "\r\n Hello$World&quot;You&quot;\r\n";
+            string pattern1 = @"\r\n";
+            string pattern2 = @"\$";
+            string pattern3 = @"&quot;";
+            IProcessor sample1 = new StringProcessor(pattern1, 0, "+");
+            IProcessor sample2 = new StringProcessor(pattern2, 1, " ");
+            IProcessor sample3 = new StringProcessor(pattern3, 2, " ' ");
+
+            TestSubject.AddProcessor(sample1);
+            TestSubject.AddProcessor(sample2);
+            TestSubject.AddProcessor(sample3);
+
+            string resultOfProcessing = TestSubject.Process(input);
+
+            Assert.Equal("+ HelloWorld ' You ' +", resultOfProcessing);
+        }
     }
 }
