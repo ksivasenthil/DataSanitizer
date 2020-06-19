f65d4ea38a2267bc44455c86f368abc2f65dadf3
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2020-06-19 18:49:47 +0530

    # Code update and addition of files * added newtonsoft json library * added a sample file * modified client code to parse json

--- Sanitizer/Program.cs
+++ Sanitizer/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
-
-namespace Sanitizer
+﻿namespace Sanitizer
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft;
+    using Newtonsoft.Json.Linq;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public string PathOfFileToSaintize = @".\SampleData\InsuranceTerms.json";
+        public string DirtyFileText()
         {
-            Console.WriteLine("Hello World!");
+            string fileContent = File.ReadAllText(PathOfFileToSaintize);
+            return fileContent;
+        }
+
+        public static void Main(string[] args)
+        {
+            Program client = new Program();
+            string jsonText = client.DirtyFileText();
+            JObject data = JObject.Parse(jsonText);
+            JObject modifiedData = new JObject();
+            foreach (KeyValuePair<string, JToken> item in data)
+            {
+                string key = item.Key;
+                Console.WriteLine(key);
+                modifiedData.Add(key.Trim(), data[key]);
+            }
+
+            foreach (KeyValuePair<string, JToken> item in modifiedData)
+            {
+                string key = item.Key;
+                Console.WriteLine(key);
+            }
         }
     }
 }
